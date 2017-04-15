8de96bd7f35853879216e5f71d553ad4384b5b59
Dhwty <thoth@groundline.net>
2017-04-15 17:00:41 -0600

    Fix logging. Move a buncha shit to config.

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const sql = require('sqlite');
-var moment = require('moment-timezone');
+const moment = require('moment-timezone');
 const schedule = require('node-schedule');
 const config = require('./config.json');
 
 console.log("Preparing Orana...\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
 
-sql.open('./fanchat.sqlite');
+sql.open(config.dbfile).catch(() => {console.error;});
 client.login(config.token);
 
 client.on('error', (e) => console.error(e));
@@ -15,8 +15,9 @@ client.on('warn', (e) => console.warn(e));
 //client.on('debug', (e) => console.info(e));
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.username}!`);
-	//Begin timer system
+	loginTime = new Date();
+	console.log(moment.tz(loginTime, config.moTZ).toString() + `: Logged in as ${client.user.username}!`);
+	//Begin scheduling system
 	let day = (24*60*60*1000);
 	let hour = (60*60*1000);
 	let hh = (30*60*1000);
@@ -24,8 +25,9 @@ client.on('ready', () => {
 	let tm = (10*60*1000);
 	let fm = (5*60*1000);
 	
-	let startTime = new Date(2017, 3, 22, 11, 0, 0);
+	let startTime = new Date(2017, 3, 29, 11, 0, 0);
 	console.log("Next party @ " + startTime);
+	client.channels.get(config.announce).setTopic('Next party @ '+ moment.tz(startTime, config.moTZ).toString());
 	let hourThree = new Date(startTime.getTime() + hour);
 	let hourTwo = new Date(hourThree.getTime() + hour);
 	let hourOne = new Date(hourTwo.getTime() + hour);
@@ -51,39 +53,42 @@ client.on('ready', () => {
 	var n = schedule.scheduleJob(endTime, function() {
 		client.channels.get(config.partyhard).sendMessage("The party is over! Thanks for joining us, and please come back next month!");
 	})
+});
 
-	/*var rule = new schedule.RecurrenceRule(); // Creates new Recurrence Rule
-	rule.minute = 0; // Must set to 0 or scheduled job will run every minute.
-	rule.hour = [14, 19, 20]; // Hours are based on your system's time.
-
-	var j = schedule.scheduleJob(rule, function() {
-		bot.channels.get("id", channel).sendMessage("Announcement at 2PM, 7PM, and 8PM");
-	})
-
-	//For different announcements:
-	rule.hour = 8; // Set different time. Use array like above for multiple hours.
-
-	var i = schedule.scheduleJob(rule, function() {
-		bot.channels.get("id", channel).sendMessage("Announcement at 8AM");
-	})
-	// Repeat for more announcements
-	*/
+client.on("guildMemberAdd", (member) => {
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+  member.addRole(config.soporati).catch(console.error);
 });
 
 client.on("message", (message) => {
+	//Begin logging
 	if (message.channel.id === config.partyhard){
-		var mtime = moment.tz(new Date(message.createdAt), "America/New_York").toString();
-		sql.get(`SELECT * FROM fanchat WHERE mid ='${message.id}'`).then(row => {
+		var mtime = moment.tz(new Date(message.createdAt), config.moTZ).toString();
+		sql.get(`SELECT * FROM ` + config.tablePartyhard + ` WHERE mid ='${message.id}'`).then(row => {
 			if(!row){
-				sql.run('INSERT INTO fanchat (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
+				sql.run('INSERT INTO ' + config.tablePartyhard + ' (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
 			}
 		}).catch(() => {
 			console.error;
-			sql.run('CREATE TABLE IF NOT EXISTS fanchat (mid TEXT, userId TEXT, name TEXT, msg TEXT)').then(() => {
-				sql.run('INSERT INTO fanchat (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
+			sql.run('CREATE TABLE IF NOT EXISTS ' + config.tablePartyhard + ' (mid TEXT, userId TEXT, name TEXT, msg TEXT, time TEXT)').then(() => {
+				sql.run('INSERT INTO ' + config.tablePartyhard + ' (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
 			});
 		});
 	}
+	if (message.channel.id === config.magisterium){
+		var mtime = moment.tz(new Date(message.createdAt), config.moTZ).toString();
+		sql.get(`SELECT * FROM ` + config.tableMagisterium + ` WHERE mid ='${message.id}'`).then(row => {
+			if(!row){
+				sql.run('INSERT INTO ' + config.tableMagisterium + ' (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
+			}
+		}).catch(() => {
+			console.error;
+			sql.run('CREATE TABLE IF NOT EXISTS ' + config.tableMagisterium + ' (mid TEXT, userId TEXT, name TEXT, msg TEXT, time TEXT)').then(() => {
+				sql.run('INSERT INTO ' + config.tableMagisterium + ' (mid, userId, name, msg, time) VALUES (?, ?, ?, ?, ?)', [message.id, message.author.id, message.author.username, message.content,mtime]);
+			});
+		});
+	}
+	//Begin commands
 	if (message.author.bot) return;
 	if (!message.content.startsWith(config.prefix)) return;
 	if (message.content.startsWith(config.prefix + "cah")) {
@@ -92,26 +97,50 @@ client.on("message", (message) => {
 		let pwd = args[1];
 		client.channels.get(config.announce).sendMessage(`Cards Against Rhapsody game at :link: <${url}>!\nThe password is: ${pwd}`);
 	} else
-	if (message.content.startsWith(config.prefix + "ping")) {
-		message.channel.sendMessage("Pong!");
-	} else
-	//Owner-only commands
+	//Begin owner-only commands
 	if(message.author.id !== config.ownerID) message.channel.sendMessage("Excuse me?");
 	if (message.content.startsWith(config.prefix + "shutdown")) {
 		message.channel.sendMessage("*goes to bed.*\nPlease restart the service.");
+		console.log('----- ' + moment.tz(new Date(), config.moTZ).toString() + ': Bot shutdown via command. -----');
 		client.destroy((err) => {
 			console.log(err);
 		});
 	}else
-	if (message.content.startsWith(config.prefix + "closed")) {
-		// overwrite permissions for a message author
-		client.channels.get(config.partyhard).overwritePermissions(client.guild.roles("name","patron"), {
-			SEND_MESSAGES: false
-		})
-		.then(() => console.log('Patrons booted!'))
-		.catch(console.error);
-	}else
-	if(message.content.startsWith(config.prefix + "role the bones")) {
-		message.channel.sendMessage(message.id + "..." + (config.gid).name);
-	}
+	//Public channel & Owner only
+	if(message.channel.type !== 'dm'){
+		if (message.content.startsWith(config.prefix + "closed")) {
+			message.channel.overwritePermissions(config.soporati, {
+				SEND_MESSAGES: false,
+				READ_MESSAGES: false
+			})
+			.then(() => console.log(moment.tz(new Date(), config.moTZ).toString() + ': Soporati booted!'))
+			.catch(console.error);
+			message.channel.overwritePermissions(config.patrons, {
+				SEND_MESSAGES: false,
+				READ_MESSAGES: false
+			})
+			.then(() => console.log(moment.tz(new Date(), config.moTZ).toString() + ': Patrons booted!'))
+			.catch(console.error);
+			message.channel.sendMessage("Party over. Guests cleared.");
+		}
+		if (message.content.startsWith(config.prefix + "open")) {
+			message.channel.overwritePermissions(config.soporati, {
+				SEND_MESSAGES: true,
+				READ_MESSAGES: true
+			})
+			.then(() => console.log(moment.tz(new Date(), config.moTZ).toString() + ': Soporati invited!'))
+			.catch(console.error);
+			message.channel.overwritePermissions(config.patrons, {
+				SEND_MESSAGES: true,
+				READ_MESSAGES: true
+			})
+			.then(() => console.log(moment.tz(new Date(), config.moTZ).toString() + ': Patrons invited!'))
+			.catch(console.error);
+			message.channel.sendMessage("Party started. Channel open to guests.");
+		}
+	}else message.channel.sendMessage("This command can't be used in private messages.");
+});
+
+client.on('disconnect', function(erMsg, code) {
+    console.log('----- ' + moment.tz(new Date(), config.moTZ).toString() + ':  Bot disconnected from Discord with code', code, '-----');
 });
